ae9b3bc778fa101e110b94705d047841d3acc104
Dillon Chanis <dillonchanis92@gmail.com>
2018-01-26 20:12:23 -0500

    Finishing tests for CRUD actions.

--- test/routes.books.test.js
+++ test/routes.books.test.js
@@ -65,6 +65,115 @@ describe('routes : movies', () => {
     })
   })
 
+  describe('POST /api/v1/books', () => {
+    it('should return the book that was posted', (done) => {
+      chai.request(server)
+        .post('/api/v1/books')
+        .send({
+          name: 'You Don\'t Know JS',
+          author: 'Kyle Simpson',
+          genre: 'Technology',
+          rating: 5
+        })
+        .end((err, res) => {
+          should.not.exist(err)
+          res.status.should.equal(201)
+          res.type.should.equal('application/json')
+          res.body.status.should.eql('success')
+          res.body.data[0].should.include.keys(
+            'id', 'name', 'author', 'genre', 'rating'
+          )
+          done()
+        })
+    })
+
+    it('should throw an error if the payload is incomplete', (done) => {
+      chai.request(server)
+        .post('/api/v1/books')
+        .send({
+          name: 'Pale Blue Dot'
+        })
+        .end((err, res) => {
+          should.exist(err)
+          res.status.should.equal(400)
+          res.type.should.equal('application/json')
+          res.body.status.should.eql('error')
+          should.exist(res.body.message)
+          done()
+        })
+    })
+  })
+
+  describe('PUT /api/v1/books', () => {
+    it('should return the book that was updated', (done) => {
+      knex('books')
+        .select('*')
+        .then((book) => {
+          const bookObject = book[0]
+          chai.request(server)
+            .put(`/api/v1/books/${bookObject.id}`)
+            .send({
+              rating: 0
+            })
+            .end((err, res) => {
+              should.not.exist(err)
+              res.status.should.equal(201)
+              res.type.should.equal('application/json')
+              res.body.status.should.eql('success')
+              res.body.data[0].should.include.keys(
+                'id', 'name', 'author', 'genre', 'rating'
+              )
+
+              const updatedBook = res.body.data[0]
+              updatedBook.rating.should.not.eql(bookObject.rating)
+              done()
+            })
+        })
+    })
+
+    it('should return an error if no book exists', (done) => {
+      chai.request(server)
+        .put('/api/v1/books/1000000000000')
+        .send({
+          rating: 0
+        })
+        .end((err, res) => {
+          should.exist(err)
+          res.status.should.equal(404)
+          res.type.should.equal('application/json')
+          res.body.status.should.eql('error')
+          res.body.message.should.eql('That book does not exist.')
+          done()
+        })
+    })
+  })
+
+  describe('DELETE /api/v1/books/:id', () => {
+    it('should return the book that was deleted', (done) => {
+      knex('books')
+        .select('*')
+        .then((books) => {
+          const firstBook = books[0]
+          const originalLength = books.length
+
+          chai.request(server)
+            .delete(`/api/v1/books/${firstBook.id}`)
+            .end((err, res) => {
+              should.not.exist(err)
+              res.status.should.equal(200)
+              res.type.should.equal('application/json')
+              res.body.status.should.eql('success')
+              res.body.data[0].should.include.keys(
+                'id', 'name', 'author', 'genre', 'rating'
+              )
+
+              knex('books').select('*')
+                .then((updatedBooks) => {
+                  updatedBooks.length.should.eql(originalLength - 1)
+                  done()
+                })
+            })
+        })
+    })
+  })
 })
-
-
